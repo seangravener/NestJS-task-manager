87cdd2ae6fbb19781ed9bde7106af291c7d15e66
Sean Gravener <sean@gravener.net>
2022-08-24 20:36:44 -0400

    Stub first controller

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { title } from 'process';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ) {
+    return this.tasksService.createTask(title, description);
+  }
+}
