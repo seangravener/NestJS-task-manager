15476cc24fa353bb47748ca7e34a37055cd5395d
Sean Gravener <sean@gravener.net>
2022-08-25 13:29:55 -0400

    Stub initial controller methods

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { title } from 'process';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { createTaskDto } from './create-task.dto';
+import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -11,11 +20,35 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string) {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string) {
+    console.log(id, this.tasksService.getAllTasks());
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+
+  // @Post()
+  // async createTask(
+  //   @Body('title') title: string,
+  //   @Body('description') description: string,
+  // ): Promise<Task> {
+  //   return await this.tasksService.createTask(title, description);
+  // }
+
   @Post()
-  createTask(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ) {
-    return this.tasksService.createTask(title, description);
+  async createTask(@Body() createTaskDto: createTaskDto): Promise<Task> {
+    return await this.tasksService.createTask(createTaskDto);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,16 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
+import { createTaskDto } from './create-task.dto';
+
+const initTasks = [
+  {
+    id: '1',
+    title: 'First task.',
+    description: 'Hard coded.',
+    status: 'OPEN',
+  },
+  {
+    id: '2',
+    title: 'Second task.',
+    description: 'Hard coded.',
+    status: 'OPEN',
+  },
+] as Task[];
 
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = [];
+  private tasks: Task[] = [...initTasks];
 
   getAllTasks(): Task[] {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => id === task.id);
+  }
+
+  createTask(createTaskDto: createTaskDto): Task {
+    const { title, description } = createTaskDto;
+
     const task: Task = {
       id: uuid(),
       title,
@@ -19,6 +41,16 @@ export class TasksService {
     };
 
     this.tasks.push(task);
+    return task;
+  }
+
+  deleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus) {
+    const task = this.getTaskById(id);
+    task.status = status;
     return task;
   }
 }
