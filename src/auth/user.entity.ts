3e7c93cbc272c5c4587c5fed530e6775dc674add
Sean Gravener <sean@gravener.net>
2022-08-28 12:10:04 -0400

    Extend Entity with BaseEntity

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,13 +1,15 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class User {
+export class User extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column({ unique: true })
   username: string;
 
+  @Exclude()
   @Column()
   password: string;
 }
